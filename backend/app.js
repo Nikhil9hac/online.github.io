983962f9bc66ee5b465d67a0b4f2eca2709334ab
Nikhil <kumarnikhilsingh.hac07@gmail.com>
2021-03-16 18:30:43 +0530

    testing3

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,21 @@
 const express=require('express');
 const app = express();
+const fs=require('fs');
 const path = require('path');
 const public=path.join(__dirname, '../public');
-app.use(express.static(public))
+app.use(express.static(public));
+const about=fs.readFile('../public/about.html','utf-8',(err,data)=>{
+    if (err) {
+        console.log(err);
+    }
+})
 
 // routing
 app.get('/',(req, res) =>{
     res.send()
 })
 app.get('/about',(req, res) =>{
-    res.send('this is about page');
+    res.send(about)
 })
 app.get('/weather',(req, res) =>{
     res.send('this is weather page');
