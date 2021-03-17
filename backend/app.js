dc089f272ecdb4523dce61a7624e0a752a47911f
Nikhil <kumarnikhilsingh.hac07@gmail.com>
2021-03-17 11:31:48 +0530

    testing4

--- backend/app.js
+++ backend/app.js
@@ -2,23 +2,30 @@ const express=require('express');
 const app = express();
 const fs=require('fs');
 const path = require('path');
-const public=path.join(__dirname, '../public');
+const public=path.join(__dirname,'../public');
+const templates=path.join(__dirname,'views/templates');
+const hbs=require('hbs')
 app.use(express.static(public));
-const about=fs.readFile('../public/about.html','utf-8',(err,data)=>{
-    if (err) {
-        console.log(err);
-    }
-})
-
+// setting templates engine  and registered templates
+app.set('view engine','hbs');
+hbs.registerPartials(templates);
 // routing
-app.get('/',(req, res) =>{
-    res.send()
-})
+// app.get('/',(req, res) =>{
+    //     res.send()
+    // })
 app.get('/about',(req, res) =>{
-    res.send(about)
+    fs.readFile('../public/about.html','utf-8',(err,data)=>{
+        if (err) {
+                console.log(err);
+        }
+        else{
+            res.send(data);
+        }
+     })
+        
 })
 app.get('/weather',(req, res) =>{
-    res.send('this is weather page');
+    res.render('weather')
 })
 app.get('*',(req, res) =>{
     res.send('oops page not found')
