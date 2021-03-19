5e0aa5eb8be1d2dfeee00500af42a5c3400c0367
Nikhil <kumarnikhilsingh.hac07@gmail.com>
2021-03-19 18:42:43 +0530

    now ready

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,13 @@
 // targeting the elements
 const right=document.querySelector('.right');
 const left=document.querySelector('.left');
+const btn1=document.getElementById("btn1")
 
 // code
 const imgArray=['bg1.jpg','bg2.jpg','bg3.jpg','bg4.jpg']
-
+btn1.addEventListener('click',()=>{
+  location.href="https://weather-app-nikhil.herokuapp.com/weather"
+})
 // timeout function
 let imgCount=0;
 setInterval(()=>{
